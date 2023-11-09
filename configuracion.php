91d95171bd876eaac2c1fe23880ba19108e0a533
ariolop154 <ariolop154@g.educaand.es>
2023-11-09 14:26:07 +0100

    Cambio de configuracion

--- configuracion.php
+++ configuracion.php
@@ -5,7 +5,7 @@ define("CLASS_PATH", dirname($_SERVER['PHP_SELF'])."/");
 define("APPLICATION_PATH", dirname($_SERVER['PHP_SELF'])."/");
 
 //Configuraci�n de la conexion a la base de datos
-define("BD_SERVIDOR", "localhost:3306");
+define("BD_SERVIDOR", "localhost");
 define("BD_NOMBRE", "peliculas");
 define ("BD_USUARIO", "admin");
 define ("BD_PASSWORD", "1234567");
