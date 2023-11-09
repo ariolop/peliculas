950c24b38a6bace7dba3b6cceb07d0f2388311ff
ariolop154 <ariolop154@g.educaand.es>
2023-11-09 14:20:17 +0100

    Cambio de configuracion

--- class.ConexionBD.php
+++ class.ConexionBD.php
@@ -14,7 +14,7 @@ class ConexionBD {
 
         
     //Constructor de la clase
-    function ConexionBD() {
+    function __construct() {
 
         $this->host = BD_SERVIDOR;
         $this->usuario = BD_USUARIO;
@@ -23,15 +23,15 @@ class ConexionBD {
     }
 
 
-    //Metodo para abrir una conexión a la base de datos
+    //Metodo para abrir una conexiï¿½n a la base de datos
     function conectar_bd() {
        $this->con=mysqli_connect($this->host,$this->usuario, $this->password, $this->bd_nombre) or die ("Error conectando a la base de datos.");
        return $this->con;
     }
 
-    //Metodo para cerrar una conexión
+    //Metodo para cerrar una conexiï¿½n
     function cerrar_conexion() {
-       mysqli_close($this->con) or die("Error al cerrar la conexión con la base de datos.");
+       mysqli_close($this->con) or die("Error al cerrar la conexiï¿½n con la base de datos.");
     }
 
 }
--- configuracion.php
+++ configuracion.php
@@ -4,18 +4,18 @@
 define("CLASS_PATH", dirname($_SERVER['PHP_SELF'])."/");
 define("APPLICATION_PATH", dirname($_SERVER['PHP_SELF'])."/");
 
-//Configuración de la conexion a la base de datos
+//Configuraciï¿½n de la conexion a la base de datos
 define("BD_SERVIDOR", "localhost:3306");
 define("BD_NOMBRE", "peliculas");
-define ("BD_USUARIO", "peliculas");
-define ("BD_PASSWORD", "peliculas");
+define ("BD_USUARIO", "admin");
+define ("BD_PASSWORD", "1234567");
 
-//Configuración de logitudes minimas del login y el password de los usuario
+//Configuraciï¿½n de logitudes minimas del login y el password de los usuario
 //normales
 define ("LONGITUD_MINIMA_LOGIN", 6);
 define ("LONGITUD_MINIMA_PASSWORD", 6);
 
-//Configuración del numero de resultados de paginación por cada pagina
+//Configuraciï¿½n del numero de resultados de paginaciï¿½n por cada pagina
 define ("PAGINACION", 10);
 
 ?>
